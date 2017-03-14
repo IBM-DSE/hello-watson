6bdd52879b29a4c07df9a4ff28638ca1322d99b8
David Thomason <dlthomas@us.ibm.com>
2017-03-14 17:39:00 -0400

    More graceful error handling when Conversation vars not found

--- pipeline/conversation.js
+++ pipeline/conversation.js
@@ -5,37 +5,42 @@
 let express = require('express'),
   extend = require('util')._extend,
   router = express.Router(), // eslint-disable-line new-cap
-  vcapServices = require('vcap_services');
+  vcapServices = require('vcap_services'),
   watson = require('watson-developer-cloud');
 
 // local module requires
 const context_manager = require('./context_manager'),
   fulfillment = require('./fulfillment');
 
-// Create the service wrapper
+// Set Conversation Service config
 let conversationConfig = extend({
-  username: process.env.CONVERSATION_USERNAME || '<username>',
-  password: process.env.CONVERSATION_PASSWORD || '<password>',
+  username: process.env.CONVERSATION_USERNAME || null,
+  password: process.env.CONVERSATION_PASSWORD || null,
   version_date: '2016-09-20',
   version: 'v1'
 }, vcapServices.getCredentials('conversation'));
-let conversation = new watson.ConversationV1(conversationConfig);
-//TODO: throw error if conversation creds are not correctly set
 
-//The conversation workspace id
+// Set the conversation Workspace ID
 let workspace_id = process.env.WORKSPACE_ID || null;
-console.log('Using Workspace ID ' + workspace_id);
+
+// Create the service wrapper
+let conversation;
+if (conversation_vars_set()) {
+  try {
+    conversation = new watson.ConversationV1(conversationConfig);
+  } catch (e) {
+    console.error(e);
+  }
+} else {
+  console.log('Using Workspace ID ' + workspace_id);
+}
 
 router.post('/', function(req, res) {
 
-  if (!workspace_id) {
-    console.error('WORKSPACE_ID is missing');
-    return res.json({
-      'output': {
-        'text': 'Oops! It doesn\'t look like I have been configured correctly...'
-      }
-    });
+  if (!conversation_vars_set()) {
+    return res.json({'output': {'text': 'Oops! It doesn\'t look like I have been configured correctly...'}});
   }
+
   let payload = {
     workspace_id: workspace_id,
     context: {}
@@ -57,7 +62,8 @@ router.post('/', function(req, res) {
     // Send the input to the conversation service
     conversation.message(new_payload, function(err, data) {
       if (err) {
-        console.error('conversation.message error: ' + JSON.stringify(err));
+        console.error('conversation.message error: ' + err.error);
+        if (err.description) console.error(err.description);
         return res.status(err.code || 500).json(err);
       }
       // if (logs) {
@@ -68,9 +74,26 @@ router.post('/', function(req, res) {
       // }
       fulfillment.handle_message(res, data);
     });
-
   });
-
 });
+
+function conversation_vars_set() {
+  const err_msg = 'Missing Conversation Service Var! Please set CONVERSATION_VAR';
+  const ext_msg = ' or connect your Bluemix app to a Conversation Service instance.';
+  let correct = true;
+  if (!conversationConfig.username) {
+    console.error(err_msg.replace('Var', 'Username').replace('CONVERSATION_VAR', 'CONVERSATION_USERNAME') + ext_msg);
+    correct = false;
+  }
+  if (!conversationConfig.password) {
+    console.error(err_msg.replace('Var', 'Password').replace('CONVERSATION_VAR', 'CONVERSATION_PASSWORD') + ext_msg);
+    correct = false;
+  }
+  if (!workspace_id) {
+    console.error(err_msg.replace('Var', 'Workspace ID').replace('CONVERSATION_VAR', 'WORKSPACE_ID') + ext_msg);
+    correct = false;
+  }
+  return correct;
+}
 
 module.exports = router;
--- ui/js/api.js
+++ ui/js/api.js
@@ -18,13 +18,13 @@
 
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "^Api$" }] */
 
-var Api = (function() {
+let Api = (function() {
   'use strict';
-  var userPayload;
-  var watsonPayload;
-  var context;
+  let userPayload;
+  let watsonPayload;
+  let context;
 
-  var messageEndpoint = '/api/message';
+  let messageEndpoint = '/api/message';
 
   // Publicly accessible methods defined
   return {
@@ -54,28 +54,22 @@ var Api = (function() {
 
   // Send a message request to the server
   function postConversationMessage(text) {
-    var data = {'input': {'text': text}};
+    let data = {'input': {'text': text}};
     if (context) {
       data.context = context;
     }
     Api.setUserPayload(data);
-    var http = new XMLHttpRequest();
+    let http = new XMLHttpRequest();
     http.open('POST', messageEndpoint, true);
     http.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     http.onload = function() {
       if (http.status === 200 && http.responseText) {
-        var response = JSON.parse(http.responseText);
+        let response = JSON.parse(http.responseText);
         context = response.context;
         Api.setWatsonPayload(response);
       } else {
         Api.setWatsonPayload({output: {text: [
-          'The service may be down at the moment; please check' +
-          ' <a href="https://status.ng.bluemix.net/" target="_blank">here</a>' +
-          ' for the current status. <br> If the service is OK,' +
-          ' the app may not be configured correctly,' +
-          ' please check workspace id and credentials for typos. <br>' +
-          ' If the service is running and the app is configured correctly,' +
-          ' try refreshing the page and/or trying a different request.'
+          "Oops, looks like I'm having some trouble talking to the Conversation Service..."
         ]}});
         console.error('Server error when trying to reply!');
       }
